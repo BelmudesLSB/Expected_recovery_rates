176295e49026808bc096ae640d2a9f0a2f87f153
Lucas Belmudes <belmu002@umn.edu>
2023-11-02 15:36:14 -0500

    Running for plots

--- Mex_compilation.m
+++ Mex_compilation.m
@@ -8,13 +8,13 @@ clear;
 mex CXXFLAGS="$CXXFLAGS -fopenmp -O3" LDFLAGS="$LDFLAGS -fopenmp" main.cpp economy.cpp initialization.cpp auxiliary.cpp 
 
 %% Common parameters:
-params.b_grid_size_lowr = 60;           % Number of points in the grid for the bond price.
-params.b_grid_size_highr = 100;
+params.b_grid_size_lowr = 75;           % Number of points in the grid for the bond price.
+params.b_grid_size_highr = 121;
 params.b_grid_min_lowr = -0.8;          % Minimum value of the bond price.
 params.b_grid_min_highr = -1.5;
 params.b_grid_max_lowr = 0.0;           % Maximum value of the bond price.
 params.b_grid_max_highr = 0.0;
-params.y_grid_size = 17;               % Number of points in the grid for the income.
+params.y_grid_size = 21;               % Number of points in the grid for the income.
 params.y_default = 0.969;              % Maximum income under default.
 params.beta = 0.953;                   % Discount factor.
 params.gamma = 2;                      % Risk aversion.
@@ -25,7 +25,7 @@ params.theta = 0.282;                  % Probability of a re-entry.
 params.max_iter = 1000;                 % Maximum number of iterations.
 params.tol = 1e-7;                     % Tolerance for the convergence.
 params.m = 3;                          % Number of standard deviations for the income grid.
-params.alpha_lowr = 0;                   % Low recovery on defaulted debt.
+params.alpha_lowr = 0.15;              % Low recovery on defaulted debt.
 params.alpha_highr = 0.3;               % High recovery on defaulted debt.
 
 %% Run code with both alphas;
